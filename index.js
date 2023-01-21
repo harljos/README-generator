b2a76c743bf59ed48317eb01c08a20483f21d734
unknown <joseph.harley02@gmail.com>
2023-01-21 11:55:14 -0500

    Added packages needed and added to questions array

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const markdown = require("./utils/generateMarkdown.js");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of the project?",
+        name: "Title",
+    },
+    {
+        type: "input",
+        message: "Provide a Description of the project.",
+        name: "Description",
+    },
+    {
+        type: "input",
+        message: "What license is your project using?",
+        name: "License",
+    },
+    {
+        type: "input",
+        message: "What is the usage for the project?",
+        name: "Usage",
+    },
+    {
+        type: "input",
+        message: "Who are the contributors?",
+        name: "Contributors",
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
